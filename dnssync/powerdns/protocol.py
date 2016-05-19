bbe9ef1579d83427cf129823fb2412d6abebcf36
metagriffin <mg.github@uberdev.org>
2016-05-19 11:16:11 -0400

    added wrapper around PowerDNS soap requests to auto-convert "Response.code != 100" into exceptions

--- dnssync/powerdns/protocol.py
+++ dnssync/powerdns/protocol.py
@@ -23,7 +23,74 @@
 # been deleted, but was located at:
 #   https://github.com/05bit/python-powerdnsclient
 
-from suds.client import Client as BaseClient
+from suds.client import Client as BaseClient, Method
+
+from dnssync import api
+
+#------------------------------------------------------------------------------
+
+POWERDNS_CODES = {
+  # cached from:
+  #   https://www.powerdns.net/inc/pdf/PowerDNS%20Express%20API%202.3.pdf
+  100 : ('SUCCESS',                          'Operation completed succesfully'),
+  200 : ('INVALID_USER',                     'Invalid user, API key is incorrect'),
+  210 : ('NOT_ENOUGH_BALANCE',               'Not enough balance to renew domain'),
+  300 : ('DOMAIN_NAME_INVALID',              'Not a valid domain name. All names must be in the form of <name>.<tld>'),
+  301 : ('DOMAIN_ALREADY_EXISTS',            'Domain could not be added because it already exists in PowerDNS'),
+  302 : ('DOMAIN_IS_RESERVED',               'Domain is reserved and could not be added'),
+  310 : ('NO_SUCH_DOMAIN',                   'Domain could not be found in your control panel'),
+  320 : ('ADDING_DOMAIN_FAILED',             'Domain could not be added to your control panel'),
+  330 : ('DELETING_DOMAIN_FAILED',           'Domain could not be deleted from your control panel'),
+  400 : ('RECORD_NOT_VALID_FOR_THIS_DOMAIN', 'Record is not valid. Record must belong to the domain you are adding it to.'),
+  401 : ('RECORD_INVALID_TTL',               'TTL is invalid. It must be higher than 60.'),
+  402 : ('RECORD_INVALID_TYPE',              'Invalid type of record. Allowed types are: "URL", "NS", "A", "AAAA", "CNAME", "PTR", "MX" and "TXT"'),
+  403 : ('RECORD_INVALID_PRIORITY',          'Invalid priority. It must be higher than 0.'),
+  404 : ('RECORD_INVALID_NAME',              'Invalid record name. Record must be in the form of <sub>.<domain>.<tld>.'),
+  410 : ('NO_SUCH_RECORD',                   'Record was not found'),
+  420 : ('ADDING_RECORD_FAILED',             'Record could not be added to the zone'),
+  430 : ('DELETING_RECORD_FAILED',           'Record could not be deleted from the zone'),
+  440 : ('EDITING_RECORD_FAILED',            'Record could not be updated'),
+}
+
+POWERDNS_CODE_SUCCESS   = 100
+POWERDNS_CODE_AUTHERR   = 200
+
+#------------------------------------------------------------------------------
+class WrapService(object):
+  '''
+  A wrapper around a SOAP service to check the ``Response.code``
+  result and if not equal to ``100``, throws an exception. Why
+  PowerDNS can't just use standard soap exceptions is beyond me.
+  '''
+
+  #----------------------------------------------------------------------------
+  def __init__(self, client, service, *args, **kw):
+    super(WrapService, self).__init__(*args, **kw)
+    self.client  = client
+    self.service = service
+
+  #----------------------------------------------------------------------------
+  def __getattr__(self, key):
+    # todo: i'm *sure* there is a cleaner way of doing this...
+    if key in self.__dict__:
+      return self.__dict__[key]
+    attr = getattr(self.service, key)
+    if isinstance(attr, Method):
+      def _makeCheckResponse(func):
+        def _checkResponse(*args, **kw):
+          resp = func(*args, **kw)
+          if resp.code != POWERDNS_CODE_SUCCESS:
+            msg = resp.description
+            if resp.code in POWERDNS_CODES and msg == POWERDNS_CODES[resp.code][0]:
+              msg = '[%s] %s' % (msg, POWERDNS_CODES[resp.code][1])
+            if resp.code == POWERDNS_CODE_AUTHERR:
+              raise api.AuthenticationError('%s: %s' % (resp.code, msg))
+            raise api.DriverError('%s: %s' % (resp.code, msg))
+          return resp
+        return _checkResponse
+      attr = _makeCheckResponse(attr)
+    self.__dict__[key] = attr
+    return attr
 
 #------------------------------------------------------------------------------
 class Client(BaseClient):
@@ -59,6 +126,9 @@ class Client(BaseClient):
     url = self.POWERDNS_URL
     location = '%s&apikey=%s' % (url, apikey)
     super(Client, self).__init__(url, location=location)
+    self._service = self.service
+    self.service = WrapService(self, self.service)
+
 
 #------------------------------------------------------------------------------
 # end of $Id$
